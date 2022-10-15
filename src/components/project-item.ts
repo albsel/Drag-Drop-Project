7a573dfdade4222bb432c9261c4130bfc87410d9
albsel <alban_selimi@yahoo.com>
2022-10-15 21:35:22 +0200

    webpack-version

---
+++ src/components/project-item.ts
@@ -0,0 +1,49 @@
+import { Dragable } from "../models/drag-drop";
+import { Component } from "./base-component";
+import { autobind } from "../decorators/autobind";
+import { Project } from "../models/project";
+
+// ProjectItem class
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLElement>
+  implements Dragable
+{
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostid: string, project: Project) {
+    super("single-project", hostid, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
+
+  @autobind
+  dragEndHandler(_: DragEvent) {}
+
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
